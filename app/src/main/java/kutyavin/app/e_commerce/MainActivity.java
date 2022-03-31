636e1eb791997bea6d9b6fb20e4106a9c7471638
Максим <exclusivejester@gmail.com>
2022-03-31 21:28:07 +0300

    e-commerce

---
+++ app/src/main/java/kutyavin/app/e_commerce/MainActivity.java
@@ -0,0 +1,66 @@
+package kutyavin.app.e_commerce;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import kutyavin.app.e_commerce.adapter.CategoryAdapter;
+import kutyavin.app.e_commerce.adapter.CourseAdapter;
+import kutyavin.app.e_commerce.model.Category;
+import kutyavin.app.e_commerce.model.Course;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView categoryRecycler, courseRecycler;
+    CategoryAdapter categoryAdapter;
+    CourseAdapter courseAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        List<Category> categoryList = new ArrayList<>();
+        categoryList.add(new Category(1,"Игры"));
+        categoryList.add(new Category(2,"Сайты"));
+        categoryList.add(new Category(3,"Языки"));
+        categoryList.add(new Category(4,"Прочее"));
+
+        setCategoryRecycler(categoryList);
+
+        List<Course> courseList = new ArrayList<>();
+        courseList.add(new Course(1,"java", "Профессия Java\nразработчик", "1 января", "начальный", "#424345", "Test"));
+        courseList.add(new Course(2,"python", "Профессия Python\nразработчик", "10 января", "начальный", "#9FA52D", "Test"));
+
+        setCourseRecycler(courseList);
+    }
+
+    private void setCourseRecycler(List<Course> courseList) {
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false);
+
+        courseRecycler = findViewById(R.id.courseRecycler);
+        courseRecycler.setLayoutManager(layoutManager);
+
+        courseAdapter = new CourseAdapter(this, courseList);
+        courseRecycler.setAdapter(courseAdapter);
+
+    }
+
+    private void setCategoryRecycler(List<Category> categoryList) {
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false);
+
+        categoryRecycler = findViewById(R.id.categoryRecycler);
+        categoryRecycler.setLayoutManager(layoutManager);
+
+        categoryAdapter = new CategoryAdapter(this, categoryList);
+        categoryRecycler.setAdapter(categoryAdapter);
+
+    }
+}
